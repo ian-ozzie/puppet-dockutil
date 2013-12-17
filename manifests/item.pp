7216012be5c44cb2fa8046d8aeb4a10a2d74ec15
Ian Oswald <ian@ozzie.io>
2013-12-17 17:03:18 +1100

    Added folder options and validation.

--- manifests/item.pp
+++ manifests/item.pp
@@ -5,6 +5,9 @@ define dockutil::item (
   $pos_before = undef,
   $pos_after = undef,
   $pos_value = undef,
+  $folder_view = undef,
+  $folder_display = undef,
+  $folder_sort = undef,
 ) {
   validate_re($ensure, '^(present|absent)$',
     "${ensure} is not supported for ensure.
@@ -48,8 +51,38 @@ define dockutil::item (
         $position = ''
       }
 
+      if $folder_view != undef {
+        validate_re($folder_view, '^(grid|fan|list|automatic)$',
+          "${folder_view} is not supported for folder_view.
+          Allowed values are grid, fan, list or automatic."
+        )
+        $view = "--view '${folder_view}'"
+      } else {
+        $view = ''
+      }
+
+      if $folder_display != undef {
+        validate_re($folder_display, '^(folder|stack)$',
+          "${folder_display} is not supported for folder_display.
+          Allowed values are folder or stack."
+        )
+        $display = "--display '${folder_display}'"
+      } else {
+        $display = ''
+      }
+
+      if $folder_sort != undef {
+        validate_re($folder_sort, '^(name|dateadded|datemodified|datecreated|kind)$',
+          "${folder_sort} is not supported for folder_sort.
+          Allowed values are name, dateadded, datemodified, datecreated or kind."
+        )
+        $sort = "--sort '${folder_sort}'"
+      } else {
+        $sort = ''
+      }
+
       exec { "dockutil-add-${name}":
-        command => "${boxen::config::cachedir}/dockutil/scripts/dockutil --add '${item}' --label '${name}' ${position} ${after} ${before} --no-restart",
+        command => "${boxen::config::cachedir}/dockutil/scripts/dockutil --add '${item}' --label '${name}' ${position} ${after} ${before} ${view} ${display} ${sort} --no-restart",
         onlyif  => "${boxen::config::cachedir}/dockutil/scripts/dockutil --find '${name}' | grep -qx '${name} was not found in /Users/${::luser}/Library/Preferences/com.apple.dock.plist'";
       }
     }
